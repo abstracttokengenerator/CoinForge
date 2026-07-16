a5750ccc401a36f3b7918019b82241e9bad3ac8d
quibiq-cmo <59870186+quibiq-cmo@users.noreply.github.com>
2026-07-16 19:13:49 +0200

    revert: restore original provider config (was working fine)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { AbstractWalletProvider } from '@abstract-foundation/agw-react'
-import { abstract } from 'viem/chains'
+import { http } from 'viem'
 import { Toaster } from 'react-hot-toast'
+import { abstractMainnet } from '@/lib/config'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AbstractWalletProvider chain={abstract}>
+    <AbstractWalletProvider
+      chain={abstractMainnet}
+      transport={http('https://api.mainnet.abs.xyz')}
+    >
       {children}
       <Toaster
         position="bottom-right"
